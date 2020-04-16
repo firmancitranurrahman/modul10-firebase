e6cc8b2d0888a1e81b8bacfc8b1223ce0bf449d0
firmancitranurrahman <firmancitranurrahman@gmail.com>
2020-04-16 16:09:27 +0700

    finish

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+export default{
+    
+        apiKey: "AIzaSyCQgOtbHUWs3Bwy47WlzgfyD6nQdqLgeUo",
+        authDomain: "modul10-firebase.firebaseapp.com",
+        databaseURL: "https://modul10-firebase.firebaseio.com",
+        projectId: "modul10-firebase",
+        storageBucket: "modul10-firebase.appspot.com",
+        messagingSenderId: "376117661499",
+        appId: "1:376117661499:web:14cfde2a6bc019e77c711e",
+        measurementId: "G-ZRH9WZ80N7"
+      
+};
